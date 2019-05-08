2724ad282895308367d412fb35be0fb459329569
jcgalvis <jcgalvis6@hotmail.com>
2019-05-07 20:08:32 -0500

    Crear función mover torre.

--- movimientos.py
+++ movimientos.py
@@ -40,4 +40,41 @@ def obtener_nombre_pieza(simbolo):
         return 'No es una pieza'
 
 def mover_torre(tablero, x_inicial, y_inicial, x_final, y_final):
-    
+    print(tablero)
+    print(x_inicial)
+    print(y_inicial)
+    print(x_final)
+    print(y_final)
+    esTorre = True
+    if (x_inicial == x_final or y_inicial == y_final):
+        esTorre = tablero[x_inicial][y_inicial] in 'tT'
+
+        if (esTorre):
+            for x in range(x_inicial + 1, x_final):
+                if (tablero[x][y_inicial] == ' '):
+                    for y in range(y_inicial, y_final):
+                        print(tablero[x_inicial][y])
+                else:
+                    print('Camino bloqueado,')
+                    break
+
+        else:
+            print('No es una torre.')
+    else:
+        print('El movimiento no es valido')
+
+
+if __name__ == '__main__':
+    tablero = [
+                [['t', 'k', 'a', 'q', 'r', 'a', 'k', 't'],
+                [' ', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+                ['T', 'K', 'A', 'R', 'Q', 'A', 'K', 'T']],
+                0, 0, 4, 0
+               ]
+
+    mover_torre(tablero[0], tablero[1], tablero[2] , tablero[3], tablero[4])
--- tests.py
+++ tests.py
@@ -14,3 +14,19 @@ class Test_movimientos(TestCase):
         espero = 'Peon blanco'
         obtengo = obtener_nombre_pieza(dado)
         self.assertEquals(espero, obtengo)
+
+    def test_mover_torre(self):
+        dado = [
+                [['t', 'k', 'a', 'q', 'r', 'a', 'k', 't'],
+                ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+                ['P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'],
+                ['T', 'K', 'A', 'R', 'Q', 'A', 'K', 'T']],
+                0, 0, 3, 3
+               ]
+        espero = []
+        obtengo = mover_torre(dado[0], dado[1], dado[2] , dado[3], dado[4])
+        self.assertEquals(espero, obtengo)
